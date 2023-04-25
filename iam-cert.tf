5f06ca3556752d18baa19c87d279f37ec345c813
jason <jason.m.doyle1@gmail.com>
2023-04-25 15:40:58 -0400

    push

--- iam-cert.tf
+++ iam-cert.tf
@@ -2,7 +2,7 @@ resource "aws_iam_server_certificate" "alb-cert" {
     name_prefix = "bboys-alb-cert"
     certificate_body = file("certificate.pem")
     private_key = file("key.pem")
-    lifecylce {
+    lifecycle {
         create_before_destroy = true
     }
 }
