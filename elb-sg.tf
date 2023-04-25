6d6855886263663b1f4e9dd679cf4f9a6120502d
jason <jason.m.doyle1@gmail.com>
2023-04-25 16:03:35 -0400

    push

--- elb-sg.tf
+++ elb-sg.tf
@@ -23,6 +23,16 @@ resource "aws_security_group_rule" "elb-sg-ingress-443-from-internet" {
     cidr_blocks = ["0.0.0.0/0"]
 }
 
+resource "aws_security_group_rule" "elb-sg-ingress-80-from-internet" {
+    description = "ingress on 80 from internet"
+    type = "ingress"
+    security_group_id = aws_security_group.bboys-elb-sg.id
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+}
+
 resource "aws_security_group_rule" "elb-sg-egress-80-to-ec2" {
   description = "80 outbound from elb to ec2"
   type = "egress"
